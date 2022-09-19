3d0e16a3a649a87718514b372c863244fa7b89b0
chen-semu <chensemu@gmail.com>
2022-09-19 19:05:04 +0300

    first-commit

---
+++ src/components/features/Router/Router.jsx
@@ -0,0 +1,12 @@
+import "./Router.css";
+import React from "react";
+
+function template() {
+  return (
+    <div className="router">
+      <h1>Router</h1>
+    </div>
+  );
+};
+
+export default template;
